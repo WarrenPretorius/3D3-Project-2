5d30a8745ac9b833b54de8b00fefc51d23d4a1db
Warren Pretorius <pretoriw@tcd.ie>
2018-03-03 18:07:28 +0000

    Warren: Added some code for setting up the router socket

--- my-router.cpp
+++ my-router.cpp
@@ -1,9 +1,35 @@
-#include <string.h>
-#include <iostream>
+#include <string.h>  // For using strings
+#include <iostream>  // For input/ output to console
+#include <netinet/in.h>     // For defining router address
+#include <stdio.h>      // For perror
+#include <arpa/inet.h>  // To use inet_addr for binding
+
 
 using namespace std;
 
 int main(){
+    // Following example from http://matrixsust.blogspot.ie/2011/10/udp-server-client-in-c.html
+    
+    int sock, bytes_read, addr_len;
+    int port_num = 5000;                    // Set the port number
+    char send_data[1024], recv_data[1024];
+    struct sockaddr_in router_addr;
+
+    if( ( sock = socket( AF_INET, SOCK_DGRAM, 0 ) ) == -1 ){
+        perror("Problem opening Socket");
+        exit(1);
+    }
+
+    router_addr.sin_family = AF_INET;       // IPv4
+    router_addr.sin_port = htons(port_num); // Use network byte order
+    router_addr.sin_addr.s_addr = inet_addr("127.0.0.1");   // To bind socket to "localhost"
+    memset(router_addr.sin_zero, '\0', sizeof(router_addr.sin_zero));   // Clear memory
+    
+    int addr_len = sizeof(router_addr);
+    if( bind( sock, (struct sockaddr *)&router_addr, addr_len ) == -1 ){
+        perror( "Error binding" );
+        exit(1);
+    }
 
     return 0;
 }
