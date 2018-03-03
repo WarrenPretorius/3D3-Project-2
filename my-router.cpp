673f2244b1712814b3d7200e5c231c5b84beed9f
Warren Pretorius <pretoriw@tcd.ie>
2018-03-03 21:46:29 +0000

    Warren: few small things

--- my-router.cpp
+++ my-router.cpp
@@ -1,8 +1,10 @@
-#include <string.h>  // For using strings
-#include <iostream>  // For input/ output to console
+#include <string.h>         // For using strings
+#include <iostream>         // For input/ output to console
 #include <netinet/in.h>     // For defining router address
-#include <stdio.h>      // For perror
-#include <arpa/inet.h>  // To use inet_addr for binding
+#include <stdio.h>          // For perror
+#include <arpa/inet.h>      // To use inet_addr for binding
+#include <sys/types.h>      // For sockets & binding
+#include <sys/socket.h>     // Also sockets & binding
 
 
 using namespace std;
@@ -15,8 +17,8 @@ int main(){
     char send_data[1024], recv_data[1024];
     struct sockaddr_in router_addr;
 
-    if( ( sock = socket( AF_INET, SOCK_DGRAM, 0 ) ) == -1 ){
-        perror("Problem opening Socket");
+    if( ( sock = socket( AF_INET, SOCK_DGRAM, 0 ) ) == -1 ){    // Set sock to socket descriptor
+        perror("Problem opening Socket");                       // using IPv4 and UDP
         exit(1);
     }
 
@@ -26,10 +28,15 @@ int main(){
     memset(router_addr.sin_zero, '\0', sizeof(router_addr.sin_zero));   // Clear memory
     
     int addr_len = sizeof(router_addr);
-    if( bind( sock, (struct sockaddr *)&router_addr, addr_len ) == -1 ){
+    if( bind( sock, (struct sockaddr *)&router_addr, addr_len ) == -1 ){    // Bind socket to port
         perror( "Error binding" );
         exit(1);
     }
+
+    cout << "Router online. Accepting data on port " << port_num << endl;
+    fflush(stdout);     // Clear output stream
+
+    // Insert code to read and send data...
 
     return 0;
 }
