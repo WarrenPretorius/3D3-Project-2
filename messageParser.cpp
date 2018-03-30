5660db895848b29711db5b5897864a7d2655dd6d
Warren Pretorius <pretoriw@tcd.ie>
2018-03-30 20:38:10 +0100

    Tidied a few things

--- messageParser.cpp
+++ messageParser.cpp
@@ -1,34 +1,34 @@
 #include "messageParser.h"
 
 void messageParser::messageParserCheck(char vBuff[]) {
-	bool n_ready = true;									//for initilization
+	bool n_ready = true;									// For initilization
 
 	if (n_ready) {
-		for (int i = 0; i < sizeof(message_type); i++) {	//message type will always consist of 2 chars: ~m or ~d
-			message_type[i] = vBuff[i];						//store the 1st 2 chars of the buffer into the message typr
+		for (int i = 0; i < sizeof(message_type); i++) {	// Message type will always consist of 2 chars: ~m or ~d
+			message_type[i] = vBuff[i];						// Store the 1st 2 chars of the buffer into the message typr
 		}
 	}
-	string type(message_type, sizeof(message_type));		//since array is not null terminated, need to specify length of array
+	string type(message_type, sizeof(message_type));		// Since array is not null terminated, need to specify length of array
 
-	if (type == "~m") {										//check what type of message we have
-		bool n_ready = false;								//so we don't initilize again
+	if (type == "~m") {										// Check what type of message we have
+		bool n_ready = false;								// So we don't initilize again
 		messageTypeForward(vBuff);
 	}
 
 	if (type == "~d") {
 		cout << "\nEnter";
-		bool n_ready = false;								//so we don't initilize again
+		bool n_ready = false;								// So we don't initilize again
 	}
 
-	else {													//small error here, enters this loop even if type == ~m, will fix later
+	else {													// Small error here, enters this loop even if type == ~m, will fix later
 		cout << "\n\nInvalid Type of message in Buffer\n\n";
 	}
 }
 
 void messageParser::messageTypeForward(char vBuff[]) {
-	strcpy(message_arr, vBuff);								//put everyting in vbuff into message arr
-	strcpy(message_arr2, vBuff);							//put everyting in vbuff into message arr
-	strcpy(message_arr3, vBuff);							//put everyting in vbuff into message arr
+	strcpy(message_arr, vBuff);								// Put everyting in vbuff into message arr
+	strcpy(message_arr2, vBuff);							// Put everyting in vbuff into message arr
+	strcpy(message_arr3, vBuff);							// Put everyting in vbuff into message arr
 
 	sourceSubString = strtok(message_arr, "S");
 	sourceSubString = strtok(NULL, " ");
@@ -38,14 +38,14 @@ void messageParser::messageTypeForward(char vBuff[]) {
 	destSubString = strtok(NULL, " ");
 	dest_port_num = atoi(destSubString);
 
-	substring = strtok(message_arr3, "'");					//find the first '
-	substring = strtok(NULL, "'");							//last quote
-	string message(substring);								//convert into string that we can send 
+	substring = strtok(message_arr3, "'");					// Find the first '
+	substring = strtok(NULL, "'");							// Last quote
+	string message(substring);								// Convert into string that we can send 
 
 			//TEST//
-	cout << "Source Port #: " << source_port_num << endl;	//test to see what source port num
-	cout << "Dest Port #: " << dest_port_num << endl;		//test to see what dest port num
-	cout << endl << message << endl;;						//test to see what message is
+	cout << "Source Port #: " << source_port_num << endl;	// Test to see what source port num
+	cout << "Dest Port #: " << dest_port_num << endl;		// Test to see what dest port num
+	cout << endl << message << endl;;						// Test to see what message is
 
 	
 
--- messageParser.h
+++ messageParser.h
@@ -11,18 +11,18 @@ using namespace std;
 class messageParser {
 
 private:
-	char* substring;	        //will have what is between ' '
+	char* substring;	        // Will have what is between ' '
 	char* sourceSubString;
 	char* destSubString;
-	char message_arr[1024];	    //holds the everything that is in vBuff so we can take out the message between ' ', the size is 1024, same as vBuff
+	char message_arr[1024];	    // Holds the everything that is in vBuff so we can take out the message between ' ', the size is 1024, same as vBuff
 	char message_arr2[1024];
 	char message_arr3[1024];
-	int source_port_num;        //we will convert the array holding the port num to an int
-	int dest_port_num;          //we will convert the array holding the dest num to an int
-	char message_type[2];       //this is meant to hold ~m or ~d, stores the first 2 chars in an array, then convert that array to a string
-							    //then compare that string to what it should be (ex: ~m or ~d) and if it equals to one of those
-							    //then enter the loop for that message type. Have not implemented ~d yet. Also ~m could be improved.
-	bool n_ready = true;	    //for initilization
+	int source_port_num;        // We will convert the array holding the port num to an int
+	int dest_port_num;          // We will convert the array holding the dest num to an int
+	char message_type[2];       // This is meant to hold ~m or ~d, stores the first 2 chars in an array, then convert that array to a string
+							    // then compare that string to what it should be (ex: ~m or ~d) and if it equals to one of those
+							    // then enter the loop for that message type. Have not implemented ~d yet. Also ~m could be improved.
+	bool n_ready = true;	    // For initilization
 
 
 public:
